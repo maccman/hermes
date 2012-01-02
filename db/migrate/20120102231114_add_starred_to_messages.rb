7c9a726173c08242d1465210f29a132c5bb2f273
Alex MacCaw <maccman@gmail.com>
2012-01-02 15:19:04 -0800

    starred should default false

--- db/migrate/20120102231114_add_starred_to_messages.rb
+++ db/migrate/20120102231114_add_starred_to_messages.rb
@@ -1,5 +1,5 @@
 class AddStarredToMessages < ActiveRecord::Migration
   def change
-    add_column :messages, :starred, :boolean
+    add_column :messages, :starred, :boolean, :default => false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120102231114) do
+ActiveRecord::Schema.define(:version => 20120102230846) do
 
   create_table "attachments", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -56,7 +56,6 @@ ActiveRecord::Schema.define(:version => 20120102231114) do
     t.integer  "from_user_id"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
-    t.boolean  "starred"
   end
 
   create_table "users", :force => true do |t|
