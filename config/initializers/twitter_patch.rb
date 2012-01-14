413ec05dc825790625aad371c8f3c92c9da57387
Alex MacCaw <maccman@gmail.com>
2012-01-14 14:10:36 -0800

    add extra debugging

--- config/initializers/twitter_patch.rb
+++ config/initializers/twitter_patch.rb
@@ -1,9 +1,19 @@
-class OmniAuth::Strategies::Twitter
-  # We need to sleep 3 seconds after we have
-  # the token, as Twitter unfortunately needs
-  # time to propogate data :(
+class OmniAuth::Strategies::OAuth
+  # Stop rescuing in OAuth
   def callback_phase
-    sleep 3
+    raise OmniAuth::NoSessionError.new("Session Expired") if session['oauth'].nil?
+
+    request_token = ::OAuth::RequestToken.new(consumer, session['oauth'][name.to_s].delete('request_token'), session['oauth'][name.to_s].delete('request_secret'))
+
+    opts = {}
+    if session['oauth'][name.to_s]['callback_confirmed']
+      opts[:oauth_verifier] = request['oauth_verifier']
+    else
+      opts[:oauth_callback] = callback_url
+    end
+
+    @access_token = request_token.get_access_token(opts)
     super
   end
 end
+
