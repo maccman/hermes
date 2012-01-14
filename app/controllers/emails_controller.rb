64846f20b87fcbc0e21ca98ae2e699467772ecd7
Alex MacCaw <maccman@gmail.com>
2012-01-14 13:05:28 -0800

    styling

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -32,7 +32,7 @@ class EmailsController < ApplicationController
       body = body.split(/^-----Original Message-----/, 2)[0]
       body = body.split(/^________________________________/, 2)[0]
       body = body.split(/^On .+ wrote:(\n|\r\n)/, 2)[0]
-      body = body.split(/^-- ?(\n|\r\n)/, 2)[0]
+      body = body.split(/^---? ?(\n|\r\n)/, 2)[0]
       body.gsub!("Sent from my iPhone", "")
       body.gsub!("Sent from my BlackBerry", "")
       body.strip!
