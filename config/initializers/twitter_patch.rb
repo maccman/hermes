887b05110d7a0f983f00c8270c897868b66d8a8a
Alex MacCaw <maccman@gmail.com>
2012-01-14 14:22:18 -0800

    more debugging

--- config/initializers/twitter_patch.rb
+++ config/initializers/twitter_patch.rb
@@ -4,6 +4,8 @@ class OmniAuth::Strategies::OAuth
     raise OmniAuth::NoSessionError.new("Session Expired") if session['oauth'].nil?
 
     request_token = ::OAuth::RequestToken.new(consumer, session['oauth'][name.to_s].delete('request_token'), session['oauth'][name.to_s].delete('request_secret'))
+    
+    p request_token
 
     opts = {}
     if session['oauth'][name.to_s]['callback_confirmed']
@@ -11,9 +13,22 @@ class OmniAuth::Strategies::OAuth
     else
       opts[:oauth_callback] = callback_url
     end
+    
+    p opts
 
     @access_token = request_token.get_access_token(opts)
     super
+  
+   rescue ::Timeout::Error => e
+     fail!(:timeout, e)
+   rescue ::Net::HTTPFatalError, ::OpenSSL::SSL::SSLError => e
+     fail!(:service_unavailable, e)
+   rescue ::OAuth::Unauthorized => e
+     fail!(:invalid_credentials, e)
+   rescue ::MultiJson::DecodeError => e
+     fail!(:invalid_response, e)
+   rescue ::OmniAuth::NoSessionError => e
+     fail!(:session_expired, e)
   end
 end
 
