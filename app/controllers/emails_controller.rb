89a6a5b5c6f384e77c346af47f1670d1efc133ca
Alex MacCaw <maccman@gmail.com>
2012-01-16 14:59:45 -0800

    add support for cc

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -2,27 +2,34 @@ require 'mail'
 
 class EmailsController < ApplicationController
   skip_before_filter :keepsafe
+  skip_before_filter :verify_authenticity_token
   
   def create
     from      = params[:from]
     to        = params[:to] || params[:recipient]
+    cc        = params[:cc]
+    bcc       = params[:bcc]
     subject   = params[:subject]
     body      = params[:text] || params["stripped-text"] || params[:plain]
     headers   = params[:headers] && Mail.new(params[:headers])
-
-    to_user = User.find_by_handle!(Mail::Address.new(to).local)
     
-    message = Message.new(
-      uid:         headers.try(:message_id),
-      subject:     subject,
-      body:        strip(body),
-      in_reply_to: headers.try(:in_reply_to)
-    )
+    body      = strip(body)
+    from_user = User.for(from).first
+    to_users  = User.for([to, cc, bcc].join(','))
     
-    message.from_user = User.for(from).first
-    message.to        = [to_user]
-    message.user      = to_user
-    message.save!
+    to_users.each do |user|
+      message = Message.new(
+        uid:         headers.try(:message_id),
+        subject:     subject,
+        body:        body,
+        in_reply_to: headers.try(:in_reply_to)
+      )
+    
+      message.from_user = from_user
+      message.to        = to_users
+      message.user      = user
+      message.save!
+    end
     
     head :ok
   end
