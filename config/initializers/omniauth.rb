892938c8556bd2f8ae9cd4bc7f115b1fb1e42974
Alex MacCaw <maccman@gmail.com>
2012-01-29 08:28:48 -0800

    do not reauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV["TWITTER_CONSUMER_KEY"], ENV["TWITTER_CONSUMER_SECRET"]
-  provider :google_oauth2, ENV["GOOGLE_KEY"], ENV["GOOGLE_SECRET"], scope: "userinfo.email,userinfo.profile,https://www.google.com/m8/feeds"
+  provider :google_oauth2, ENV["GOOGLE_KEY"], ENV["GOOGLE_SECRET"], access_type: "", scope: "userinfo.email,userinfo.profile,https://www.google.com/m8/feeds"
   on_failure do |env, msg|
     [200, {}, [env["omniauth.error"].inspect + ' ' + msg.to_s]] 
   end
